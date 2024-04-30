2e078b75ae3d31c90b31629ccc57b3963a65202d
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-04-30 19:38:34 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -1,37 +1,24 @@
-let equipeBusca = []
-let quantBusca = []
-let bandeiraBusca = []
-let realizarPesquisa = false
-
+let nomeBusca = [];
+let userBusca = [];
+let serieBusca = [];
+let anoBusca = [];
 function extraindodados(dados) {
     dados.forEach((elemento, index) => {
         if (index == 0) {
             
         } else {
-            equipeBusca.push(elemento.c[0].v)
-            quantBusca.push(elemento.c[1].v)
-            bandeiraBusca.push(elemento.c[2].v)
+            nomeBusca.push(elemento.c[1].v)
+            userBusca.push(elemento.c[0].v)
+            serieBusca.push(elemento.c[2].v)
+            anoBusca.push(elemento.c[3].v)
         }
     })
 }
 async function busca() {
-    let url = `https://docs.google.com/spreadsheets/d/1Yk4UDDB2COLOjSBxRYHqwvSBu_b0tyBbOaYD4EqtLG0/gviz/tq?tqx=out:json`
+    let url = `https://docs.google.com/spreadsheets/d/1S0QjkPaL6Xfa9sLCHVrzpfB2YZZelzqB1D4Qa1WREDg/gviz/tq?tqx=out:json`
 
     let data = await fetch(url)
         .then(res => res.text())
         .then(text => JSON.parse(text.substr(47).slice(0, -2)))
     extraindodados(data.table.rows);
-}
-
-async function letsQuizz() {
-    if (realizarPesquisa == false) {
-        console.log("Pesquisa está desativada");   
-    } else {
-        while (realizarPesquisa == true) {
-            setTimeout(() => {
-                console.clear();
-                busca()
-            }, 60000);
-        }
-    }
 }
