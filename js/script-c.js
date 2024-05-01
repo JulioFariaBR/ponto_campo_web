d30063039666edc462397e6c57ffd700242291c3
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-01 13:56:57 -0300

    #

--- js/script-c.js
+++ js/script-c.js
@@ -2,7 +2,11 @@ const sectA = document.querySelector('[data-equipe="a"]');
 const sectB = document.querySelector('[data-equipe="b"]');
 
 function contruindoCadaBandeira(lista, eq) {
-    lista.forEach((element, index) => {
-        
+    posiveisBandeiras.forEach((element, index) => {
+        if (comparacao(lista, posiveisBandeiras[index]) == false) {
+            eq.innerHTML = `${eq.innerHTML} <div class="sect__div" data-bandeira="on"></div>`
+        } else {
+            eq.innerHTML = `${eq.innerHTML} <div class="sect__div" data-bandeira="off"></div>`
+        }
     });
 }
