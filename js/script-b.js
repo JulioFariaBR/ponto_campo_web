c75c5a997b2f392faed9273a794b48c3783d8032
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-01 13:19:26 -0300

    #

--- js/script-b.js
+++ js/script-b.js
@@ -4,10 +4,20 @@ let bandeirasB = []
 function contrutorDeBandeira(eq, quant, band) {
     eq.forEach((element, index) => {
         if (element == "a") {
+            
             bandeirasA.push(band[index]);
         } else if (element == "b") {
             bandeirasB.push(band[index]);
         }
     });
     console.log(bandeirasA, bandeirasB);
+}
+
+function comparacao(lista, item) {
+    let found = lista.find((element) => element == item);
+    if (found == undefined) {
+        return true
+    } else {
+        return false
+    }
 }
