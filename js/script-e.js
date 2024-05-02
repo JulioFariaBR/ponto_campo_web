c7d238d1389f2c7cd4ff8b52c79de8357e72c3ba
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-01 21:04:19 -0300

    #

--- js/script-e.js
+++ js/script-e.js
@@ -8,5 +8,11 @@ const loading = `<div class="loader"></div><h1>Não Saia da Página...</h1>`
 const complete = `<div class="ok"></div><h1>Concluído</h1><h1>Pode sair da Página...</h1>`
 
 function onInfos() {
-    body.innerHTML = loading
+    body.innerHTML = loading;
+
+    equipe = params.get("eq");
+    quant = parseInt(params.get("quant"));
+    band = parseInt(params.get("band"));
+
+    body.innerHTML = complete;
 }
