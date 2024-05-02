8da0c5dfe77c0c55b73d04fdeeef53489580dd8b
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-01 21:33:41 -0300

    #

--- js/script-f.js
+++ js/script-f.js
@@ -2,7 +2,7 @@
 
 function enviarDadosCad(dados) {
     console.log(JSON.stringify(dados));
-    fetch('https://api.sheetmonkey.io/form/xogUMhbowm4q75RAG549QR', {
+    fetch('https://api.sheetmonkey.io/form/8316xXxakHEu8WP6pHgKKK', {
         method: 'post',
         headers: {
             'Accept': 'application/json',
