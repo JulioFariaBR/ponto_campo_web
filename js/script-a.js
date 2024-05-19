2fafdde24cc50a51888e474e85b0536b11f0d525
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-19 13:33:38 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -57,20 +57,19 @@ function contruirBandeirasFalsas(eq) {
 function contrutorDeEquipes(_equipeA, _equipeB, _equipeC, _equipeD, _equipeE,) {
     if (_equipeA.find((element) => element == 0) == 0) {
         document.querySelector('[data-equipe="a"]').style.display = 'flex';
-        document.querySelector('[data-equipetexto="a"]').style.display = 'block';
+        document.querySelector('[data-equipetexto="a"]').style.display = 'block';}
     if (_equipeB.find((element) => element == 0) == 0) {
         document.querySelector('[data-equipe="b"]').style.display = 'flex';
-        document.querySelector('[data-equipetexto="b"]').style.display = 'block';
+        document.querySelector('[data-equipetexto="b"]').style.display = 'block';}
     if (_equipeC.find((element) => element == 0) == 0) {
         document.querySelector('[data-equipe="c"]').style.display = 'flex';
-        document.querySelector('[data-equipetexto="c"]').style.display = 'block';
+        document.querySelector('[data-equipetexto="c"]').style.display = 'block';}
     if (_equipeD.find((element) => element == 0) == 0) {
         document.querySelector('[data-equipe="d"]').style.display = 'flex';
-        document.querySelector('[data-equipetexto="d"]').style.display = 'block';
+        document.querySelector('[data-equipetexto="d"]').style.display = 'block';}
     if (_equipeE.find((element) => element == 0) == 0) {
         document.querySelector('[data-equipe="e"]').style.display = 'flex';
-        document.querySelector('[data-equipetexto="e"]').style.display = 'block';
-    }
+        document.querySelector('[data-equipetexto="e"]').style.display = 'block';}
 }
 
 atualizarDados();
